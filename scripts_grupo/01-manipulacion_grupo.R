afa86a0da06c2e6e040b3715850d44310fc4f2c9
Mozzi <santimozzi2@gmail.com>
2026-04-24 13:44:58 -0300

    Correcciones en script 01 y 02

--- scripts_grupo/01-manipulacion_grupo.R
+++ scripts_grupo/01-manipulacion_grupo.R
@@ -4,16 +4,21 @@
 library(tidyverse)
 #Me "traigo" los datos que voy a usar
 attach(datos)
-#Filtramos los datos con el uso de mutate()
+#Filtramos las columnas con el uso de select
 datos_limpios <- datos %>% #usamos la pipeline para asignar el resultado
                             # de una operacion
   
- # nos quedamos con las columnas de interes
-  select(academia, sec_mng, tipo_academia_es, areas_mng, mng, ane) %>%
-  
-  # nos quedamos solo con los paises con academias
-  filter(academia == "Sí")
+# nos quedamos con las columnas de interes
+  select(academia, sec_mng, tipo_academia_es, areas_mng, mng, ane, GIRAI
+         , NU_region, Pais, privado, tipo_privado_es)
     
+# Definimos el orden jerárquico de la variable ordinal sec_mng
+niveles_desarrollo <- c("Muy bajo", "Bajo", "Medio", "Alto", "Muy alto")
     
+datos_limpios <- datos_limpios %>%
+  mutate(
+    sec_mng = factor(sec_mng, levels = niveles_desarrollo, ordered = TRUE)
+  )
+
 # vemos los resultados finales
 str(datos_limpios)
--- scripts_grupo/02-analisis_tablas_grupo.R
+++ scripts_grupo/02-analisis_tablas_grupo.R
@@ -6,7 +6,7 @@ library(janitor)
 #me traigo los datos limpios
 attach(datos_limpios)
 
-#Table para categorica nominal
+#Table para categorica nominal (hay academia)
 
 tabla_academia <- datos_limpios %>%
   tabyl(academia) %>%
@@ -14,21 +14,44 @@ tabla_academia <- datos_limpios %>%
   adorn_pct_formatting(digits = 1) %>%
   rename("Presencia de Academia" = academia, "N" = n, "%" = percent)
 
+#Table para categorica nominal (hay privado)
 
-#Table para variable Ordinal
+tabla_privado <- datos_limpios %>%
+  tabyl(privado) %>%
+  adorn_totals("row")%>%
+  adorn_pct_formatting(digits = 1) %>%
+  rename("Presencia de privado" = privado, "N" = n, "%" = percent)
+
+#Table para categorica Ordinal (marcos normativos)
 
 tabla_leyes <- datos_limpios %>%
   tabyl(sec_mng) %>%
   adorn_totals("row") %>%
-  adorn_pct_formatting(difits = 1) %>%
-  rename("Nivel de Macros Normativos" = sec_mng, "Cant paises" = n, "%" = percent)
+  adorn_pct_formatting(digits = 1) %>%
+  rename("Nivel de Marcos Normativos" = sec_mng, "Cant paises" = n, "%" = percent)
 
-#table para variable de respuesta multiple
-tabla_tipo_iniciativa <- datos_limpios %>%
+#Table para cuantitativa discreta (Areas reguladas)
+tabla_areas_reguladas <- datos_limpios %>%
+  tabyl(areas_mng) %>%
+  adorn_totals("row") %>%
+  adorn_pct_formatting(digits = 1) %>%
+  rename("Cant de Areas Reguladas"  = areas_mng, "Paises" = n, "%" = percent)
+
+#table para variable de respuesta multiple (tipo de iniciativa academica)
+tabla_tipo_iniciativa_acade <- datos_limpios %>%
+  filter(academia == "Sí") %>%
   tabyl(tipo_academia_es) %>%
   arrange(desc(n)) %>%
   adorn_pct_formatting(digits = 1)%>%
   rename("Tipo de Iniciativa" = tipo_academia_es, "Cantidad con esta iniciativa" = n, "% del total" = percent)
 
+#table para variable de respuesta multiple (tipo de iniciativa privado)
+tabla_tipo_iniciativa_priv <- datos_limpios %>%
+  mutate(tipo_privado_es = replace_na(tipo_privado_es, "Sin especificar")) %>%
+  filter(privado == "Sí") %>%
+  tabyl(tipo_privado_es) %>%
+  arrange(desc(n)) %>%
+  adorn_pct_formatting(digits = 1)%>%
+  rename("Tipo de Iniciativa" = tipo_privado_es, "Cantidad con esta iniciativa" = n, "% del total" = percent)
 
 
