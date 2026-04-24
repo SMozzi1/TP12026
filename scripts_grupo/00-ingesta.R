ae8e89960558c08ff74ae33c7402ba61b343956b
bavvo9 <tobinanu@gmail.com>
2026-04-24 00:43:06 -0300

    Escritura de script 00

--- scripts_grupo/00-ingesta.R
+++ scripts_grupo/00-ingesta.R
@@ -1,1 +1,16 @@
-#Esto es una prueba de ingesta
+
+#Extracción de datos en forma de tabla desde sheet: 
+
+library(googlesheets4)
+
+# Link al archivo
+url="https://docs.google.com/spreadsheets/d/1Kwl4KByOv8q2kXMsgaO3d5QI3vUQ40RCZJgJHhg5bmE/edit?usp=sharing"
+
+# Evito loggeo
+gs4_deauth()
+
+# Leo el archivo y almaceno los datos en un data frame
+datos <- read_sheet(url, skip = 2)
+
+# Veo la estructura del dataset
+str(datos)
