e53a07c98aed8466e063d9b94f61bdacb85271d8
Mozzi <santimozzi2@gmail.com>
2026-04-24 17:43:56 -0300

    ajuste de titulos

--- scripts_grupo/03-analisis_graficos_univariados_grupo.R
+++ scripts_grupo/03-analisis_graficos_univariados_grupo.R
@@ -17,7 +17,6 @@ grafico_poblacion <- ggplot(datos_limpios,
     x = "Cantidad de paises",
     y = "Region continental"
   )
-grafico_poblacion
 
 ##############################################
 # Grafico de barra niv. de marcos normativos # (categorico ordinal)
@@ -26,8 +25,11 @@ grafico_ordinal <- ggplot(datos_limpios)+
   aes(x = sec_mng)+
   geom_bar(width = 0.75, fill = "blue", col = "black", alpha = 0.8)+
   labs(y = "Cantidad de paises", x = "Nivel de marcos normativos")+
-  ggtitle("Distribución del indice de mng sobre el uso responsable de la ia")+
+  ggtitle("Distribución del indice de mng sobre el uso 
+              responsable de la ia")+
   theme_classic()
+
+
 
 ########################################
 # Grafico de barra tipos de iniciativa # (variables de respuesta multiple)
@@ -50,10 +52,9 @@ grafico_discreta <- ggplot(datos_limpios) +
   geom_bar(width = 0.10, fill = "black") +
   scale_x_continuous(breaks = seq(1, 20, 1)) + 
   labs(y = "Cantidad de países", x = "Áreas temáticas reguladas (0 a 19)") +
-  ggtitle("Distribución de cantidad de areas tematicas por pais destinadas 
-          a la IA relacionado al Marco Nacional Gubernamental") +
+  ggtitle("Distribución de cantidad de areas tematicas por 
+    pais destinadas a la IA relacionado al MGN") +
   theme_classic()
-
 
 #############################################
 # Histograma para indice de marco normativo # (cuantitativo continuo)
@@ -63,8 +64,7 @@ grafico_continua <- ggplot(datos_limpios)+
   aes(x = mng)+
   geom_histogram(fill = "gray", col = "black", bins = 10)+
   labs(x = "Puntaje de Marcos Normativos (0-100)", y = "Cantidad de paises")+
-  ggtitle("Distribucion de los puntajes de los Marcos Nacionales Gubernamentales
-          con respecto al uso responsable de la IA")+
+  ggtitle("Distribucion de los puntajes de los Marcos Nacionales 
+    Gubernamentales con respecto al uso responsable de la IA")+
   theme_classic()
-
 
