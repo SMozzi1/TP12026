e001ec0207096e677e8566dd956d4eda722effba
Mozzi <santimozzi2@gmail.com>
2026-04-24 17:34:50 -0300

    Cambio de titulos de graficos bivariados

--- scripts_grupo/03-analisis_graficos_univariados_grupo.R
+++ scripts_grupo/03-analisis_graficos_univariados_grupo.R
@@ -5,32 +5,32 @@ library(ggplot2)
 # me traigo los datos
 attach(datos_limpios)
 
-###########################################
-# Grafico de barra dist. paises por cont. #
-###########################################
+################################################################
+# Grafico de barra dist. paises por cont. (categorico nominal) #
+################################################################
 grafico_poblacion <- ggplot(datos_limpios, 
                             aes(y = fct_infreq(NU_region)))+
   geom_bar(fill = "red", color = "black", alpha = 0.8)+
   theme_minimal()+
   labs(
-    title = "Poblacion del Estudio: Distribucion por continente",
+    title = "Distribucion de cantidad de paises por continente",
     subtitle = "Total de 138 paises segun el estandar M49 de Naciones Unidas",
     x = "Cantidad de paises",
     y = "Region continental"
   )
 
 ##############################################
-# Grafico de barra niv. de marcos normativos #
+# Grafico de barra niv. de marcos normativos # (categorico ordinal)
 ##############################################
 grafico_ordinal <- ggplot(datos_limpios)+
   aes(x = sec_mng)+
   geom_bar(width = 0.75, fill = "blue", col = "black", alpha = 0.8)+
   labs(y = "Cantidad de paises", x = "Nivel de marcos normativos")+
-  ggtitle("Nivel de desarrollo legal")+
+  ggtitle("Distribución del indice de mng sobre el uso responsable de la ia")+
   theme_classic()
 
 ########################################
-# Grafico de barra tipos de iniciativa #
+# Grafico de barra tipos de iniciativa # (variables de respuesta multiple)
 ########################################
 
 grafico_resp_multiple <- ggplot(tabla_tipo_iniciativa_acade) + 
@@ -43,24 +43,28 @@ grafico_resp_multiple <- ggplot(tabla_tipo_iniciativa_acade) +
   theme_classic()
 
 ##################################################
-# Grafico de barra para areas reguladas por pais #
+# Grafico de barra para areas reguladas por pais # (cuantitativo discreto)
 ##################################################
 grafico_discreta <- ggplot(datos_limpios) +
   aes(x = areas_mng) + 
   geom_bar(width = 0.10, fill = "black") +
   scale_x_continuous(breaks = seq(1, 20, 1)) + 
   labs(y = "Cantidad de países", x = "Áreas temáticas reguladas (0 a 19)") +
-  ggtitle("Áreas reguladas por país") +
+  ggtitle("Distribución de cantidad de areas tematicas por pais destinadas 
+          a la IA relacionado al Marco Nacional Gubernamental") +
   theme_classic()
 
+
 #############################################
-# Histograma para indice de marco normativo #
+# Histograma para indice de marco normativo # (cuantitativo continuo)
 #############################################
 
 grafico_continua <- ggplot(datos_limpios)+
   aes(x = mng)+
   geom_histogram(fill = "gray", col = "black", bins = 10)+
   labs(x = "Puntaje de Marcos Normativos (0-100)", y = "Cantidad de paises")+
-  ggtitle("Distribucion de la fuerza legislativa")+
+  ggtitle("Distribucion de los puntajes de los Marcos Nacionales Gubernamentales
+          con respecto al uso responsable de la IA")+
   theme_classic()
+
 
--- scripts_grupo/04-analisis_graficos_bivariados.R
+++ scripts_grupo/04-analisis_graficos_bivariados.R
@@ -6,7 +6,7 @@ library(ggplot2)
 attach(datos_limpios)
 
 ##################################################
-# Grafico de barra marcos normativos vs academia #
+# Grafico de barra marcos normativos vs academia # (categorica vs categorica)
 ##################################################
 grafico_cat_cat <- datos_limpios %>%
   ggplot() +
@@ -24,21 +24,39 @@ grafico_cat_cat <- datos_limpios %>%
 grafico_cat_cat
 
 ################################################################
-# Diagrama de dispersión  fuerza de la ley vs puntaje de girai #
+# Diagrama de dispersión  fuerza de la ley vs puntaje de GIRAI # (continua vs continua)
 ################################################################
 
 grafico_cont_cont <- ggplot(datos_limpios) +
   aes(x = mng, y = GIRAI) +
   geom_point(color = "darkblue", alpha = 0.6) +
   geom_smooth(method = "lm", se = FALSE, color = "forestgreen") +
-  labs(x = "Fuerza de la Ley (0-100)", y = "Puntaje Global GIRAI (0-100)") +
+  labs(x = "Puntaje de fuerza de la Ley (0-100)", y = "Puntaje Global GIRAI (0-100)") +
   ggtitle("Relación entre Legislación y Éxito en IA Responsable") +
   theme_bw()
 
 grafico_cont_cont
 
-################################################################
-# Diagrama de dispersión  fuerza de la ley vs puntaje de girai #
-################################################################
+#####################################################
+# Grafico de barras de academia vs puntaje de GIRAI #
+#####################################################
 
+# Usamos la tubería para agrupar y calcular el promedio solo para el gráfico
+grafico_cat_cont <- ggplot(datos) +
+  aes(x = academia, y = GIRAI) +
+  geom_boxplot(outlier.shape = NA, fill = "lightgray") + # Ocultamos los outliers de la caja porque ya los va a dibujar el jitter
+  theme_classic()
+
+grafico_cat_cont
+
+
+grafico_cat_cont_crestas <- ggplot(datos_limpios) +
+  aes(x = GIRAI, y = academia, fill = academia) +
+  geom_density_ridges() +
+  theme_ridges() + 
+  theme(legend.position = "none") +
+  labs(x = "Puntaje Global GIRAI (0-100)",
+       y = "¿Hay academia activa?")
+
+grafico_cat_cont_crestas
   
